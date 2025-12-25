904d3f875fa956b39949093a15bf25769ae8d643
josandonas <josandonas@outlook.com>
2025-12-25 11:46:44 -0400

    dell all: recomeçando do zero o projeto

--- models/Categoria.ts
+++
@@ -1,8 +0,0 @@
-import { Timestamp } from "firebase/firestore";
-
-export interface Categoria {
-  id: string;
-  nome: string;
-  criado_em: Timestamp;
-  ativo: boolean;
-}
--- models/Prato.ts
+++
@@ -1,8 +0,0 @@
-export interface Prato {
-  id: string;
-  nome: string;
-  preco: number;
-  categoria: string;
-  descricao: string;
-  ativo: boolean;
-}
